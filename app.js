1c36bfd0a343fc27de3dc8b2b2b31ffeec5ce535
Matthieu Mota <matthieumota@gmail.com>
2022-12-16 12:17:15 +0100

    Put value in cell and check not empty

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Variables nécessaires pour le jeu
+let gameState = ['', '', '', '', '', '', '', '', ''];
 // Ecouter le clic sur chacune des cases
 document.querySelectorAll('.cell').forEach(function (cell) {
     cell.addEventListener('click', handleClick);
@@ -8,5 +10,13 @@ function handleClick(event) {
     let cell = event.target; // Récupère la case cliquée
     let index = cell.dataset.cell; // Récupère la valeur du data-cell=
 
-    console.log(index);
+    // Vérifier si la case n'est pas vide
+    if (gameState[index] != '') {
+        return; // On stoppe le code et rien ne se passe si la case est remplie
+    }
+
+    // On remplit le tableau pour suivre l'état du jeu
+    gameState[index] = 'X';
+    // On mets à jour "l'interface" du jeu
+    cell.innerHTML = 'X';
 }
