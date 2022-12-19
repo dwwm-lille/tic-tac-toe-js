d8e7b795edb34ea318027184f9784e7f5347384c
Matthieu Mota <matthieumota@gmail.com>
2022-12-19 09:13:12 +0100

    Check win and nul game

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 // Variables nécessaires pour le jeu
 let gameState = ['', '', '', '', '', '', '', '', ''];
 let currentPlayer = 'X';
+let wins = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // Horizontales
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // Verticales
+    [0, 4, 8], [2, 4, 6], // Diagonales
+];
+let statusElement = document.querySelector('.status');
+
+// Le premier 4 se récupère comme ça
+// console.log(wins[1][1]);
 
 // Ecouter le clic sur chacune des cases
 document.querySelectorAll('.cell').forEach(function (cell) {
@@ -22,6 +31,9 @@ function handleClick(event) {
     // On mets à jour "l'interface" du jeu
     cell.innerHTML = currentPlayer;
 
+    // Vérifier si le jeu est gagné ou nul
+    checkEndGame();
+
     // Changer le joueur actuel (Si c'est X, il devient O sinon il devient X)
     currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
 
@@ -30,4 +42,38 @@ function handleClick(event) {
     // } else {
     //     currentPlayer = 'X';
     // }
+}
+
+// Fonction qui nous permet de vérifier la fin du jeu
+function checkEndGame() {
+    let winRound = false;
+
+    // Possibilités pour gagner (index avec la même valeur)
+    // 0, 1, 2 ; 3, 4, 5 ; 6, 7, 8 // Horizontales
+    // 0, 3, 6 ; 1, 4, 7 ; 2, 5, 8 // Verticales
+    // 0, 4, 8 ; 2, 4, 6 // Diagonales
+    for (let win of wins) {
+        let indexA = win[0]; // 3 par exemple pour la 2ème itération
+        let indexB = win[1]; // 4 par exemple pour la 2ème itération
+        let indexC = win[2]; // 5 par exemple pour la 2ème itération
+
+        if (gameState[indexA] == gameState[indexB] && gameState[indexB] == gameState[indexC] && gameState[indexA] != '') {
+            winRound = true;
+        }
+        // console.log(`gameState[${indexA}] == gameState[${indexB}] && gameState[${indexB}] == gameState[${indexC}] && gameState[${indexA}] != ''`);
+    }
+
+    if (gameState[0] == gameState[1] && gameState[1] == gameState[2] && gameState[0] != '') {
+        // console.log('GAGNE');
+    } else if (gameState[3] == gameState[4] && gameState[4] == gameState[5] && gameState[3] != '') {
+        // console.log('GAGNE');
+    }
+
+    if (winRound) { // Si le joueur current a gagné
+        statusElement.innerHTML = `Le joueur ${currentPlayer} a gagné !`;
+    } else if (!gameState.includes('')) { // Si le tableau n'a aucune case vide, match nul
+        statusElement.innerHTML = `Partie nulle !`;
+    }
+
+    console.log(gameState);
 }
